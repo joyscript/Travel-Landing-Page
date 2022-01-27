0f48ba52330d86f8f900fa319ff100d3dfeeae2a
joyscript <joyscript7@gmail.com>
2022-01-28 00:40:46 +0300

    fix: some little changes

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
-const scrollHeader = () => {
-  const header = document.querySelector('.header');
+const header = document.querySelector('.header');
+const menu = document.querySelector('.header__nav');
+const burger = document.querySelector('.burger');
+const close = document.querySelector('.close');
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-      header.classList.add('active');
-    } else {
-      header.classList.remove('active');
-    }
-  });
+const handlerMenu = (e) => {
+  if (!e.target.closest('.header')) menu.classList.remove('active');
 };
 
-const showMenu = () => {
-  const menu = document.querySelector('.header__nav');
-  const burger = document.querySelector('.burger');
-  const close = document.querySelector('.close');
+close.addEventListener('click', () => {
+  menu.classList.remove('active');
+  window.removeEventListener('click', handlerMenu);
+});
 
-  burger.addEventListener('click', () => {
-    menu.classList.add('active');
-  });
+burger.addEventListener('click', () => {
+  menu.classList.add('active');
+  window.addEventListener('click', handlerMenu);
+});
 
-  close.addEventListener('click', () => {
-    menu.classList.remove('active');
-  });
-};
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+});
 
 const smoothScroll = () => {
   const menulinks = document.querySelectorAll('.header__menu-link');
@@ -37,20 +38,20 @@ const smoothScroll = () => {
 
       const id = link.getAttribute('href').slice(1);
       const target = document.getElementById(id);
-      const targetPosition = target.getBoundingClientRect().top;
-      const offsetPosition = targetPosition + window.scrollY - headerHeight;
 
-      console.log(targetPosition);
-      console.log(window.scrollY);
+      if (target) {
+        const targetPosition = target.getBoundingClientRect().top;
+        const offsetPosition = targetPosition + window.scrollY - headerHeight;
 
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: 'smooth',
-      });
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth',
+        });
+      }
+
+      if (window.innerWidth <= 768) menu.classList.remove('active');
     });
   });
 };
 
-scrollHeader();
-showMenu();
 smoothScroll();
