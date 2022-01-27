46af98bd5e4df47d97879c77187fc258b8c1d6a6
joyscript <joyscript7@gmail.com>
2022-01-27 17:09:50 +0300

    feat: add adaptive styles

--- js/main.js
+++ js/main.js
@@ -10,4 +10,19 @@ const scrollHeader = () => {
   });
 };
 
+const showMenu = () => {
+  const menu = document.querySelector('.header__nav');
+  const burger = document.querySelector('.burger');
+  const close = document.querySelector('.close');
+
+  burger.addEventListener('click', () => {
+    menu.classList.add('active');
+  });
+
+  close.addEventListener('click', () => {
+    menu.classList.remove('active');
+  });
+};
+
 scrollHeader();
+showMenu();
